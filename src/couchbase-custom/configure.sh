67a405b2a3b1a52778c11190ce8dfeda782a3a02
Milena Gouveia <milenaagouveia@gmail.com>
2024-03-29 14:19:37 +0000

    Update configure.sh by adding sleep times

--- src/couchbase-custom/configure.sh
+++ src/couchbase-custom/configure.sh
@@ -2,16 +2,26 @@ set -m
 
 /entrypoint.sh couchbase-server &
 
-sleep 15
+sleep 30
 
 curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=512 -d indexMemoryQuota=512
 
+sleep 5
+
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2cn1ql%2Cindex
+
+sleep 5
 
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=$COUCHBASE_ADMINISTRATOR_USERNAME -d password=$COUCHBASE_ADMINISTRATOR_PASSWORD
 
+sleep 5
+
 curl -i -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 
+sleep 5
+
 curl -v -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD -X POST http://127.0.0.1:8091/pools/default/buckets -d name=$COUCHBASE_BUCKET -d bucketType=couchbase -d ramQuotaMB=128 -d authType=sasl -d saslPassword=$COUCHBASE_BUCKET_PASSWORD
+
+sleep 5
 
 fg 1
