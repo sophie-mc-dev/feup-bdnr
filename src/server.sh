950bd6c1ae9d4c170afb007c1a0f6ab57658facb
Milena Gouveia <milenaagouveia@gmail.com>
2024-04-07 00:38:39 +0100

    updated server.sh

--- src/server.sh
+++ src/server.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
 # Step 1: Pull Couchbase image
-echo "Step 1: Pulling Couchbase image..."
-docker pull couchbase
+docker pull couchbase/server
 
-# Step 2: Build Docker image
-echo "Step 2: Building Docker image..."
-docker build -t bdnr ./couchbase-custom
-
-# Step 3: Run Docker container
-echo "Step 3: Running Docker container..."
-docker run -d -p 8091-8093:8091-8093 \
+# Step 2: Run Docker container
+echo "Step 2: Running Docker container..."
+docker run -d --name couchbase-bdnr-g04 \
+    -p 8091-8093:8091-8093 \
     -e COUCHBASE_ADMINISTRATOR_USERNAME=Administrator \
     -e COUCHBASE_ADMINISTRATOR_PASSWORD=password \
-    -e COUCHBASE_BUCKET=default \
-    -e COUCHBASE_BUCKET_PASSWORD= \
-    --network="bridge" \
-    --name couchbase1 \
-    bdnr
+    couchbase/server
 
-# Step 4: Display Docker logs
+# Wait for Couchbase to start up
+echo "Waiting for Couchbase to start up..."
+sleep 20
+
+# Step 3: Configure Couchbase cluster
+echo "Step 3: Configuring Couchbase cluster..."
+
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=512 -d indexMemoryQuota=512
+
+sleep 5
+
+curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+
+sleep 5
+
+curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=$COUCHBASE_ADMINISTRATOR_USERNAME -d password=$COUCHBASE_ADMINISTRATOR_PASSWORD
+
+curl -i -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+
+# Step 4: Create Couchbase bucket
+echo "Step 4: Creating Couchbase bucket..."
+curl -v -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD -X POST http://127.0.0.1:8091/pools/default/buckets -d name=$COUCHBASE_BUCKET -d bucketType=couchbase -d ramQuotaMB=128 -d authType=sasl -d saslPassword=
+echo "Couchbase setup completed successfully."
+
+# Step 5: Display Docker logs
 echo "Step 4: Displaying Docker logs..."
-docker logs -f couchbase1
+docker logs -f couchbase-bdnr-g04
