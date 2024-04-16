eb71b5e2f9ce91383d37ddf1f84105ed92b6ae7a
Francisco Prada <fmamp24@gmail.com>
2024-04-17 00:28:10 +0100

    changed bucket name and completed node script

--- src/create.sh
+++ src/create.sh
@@ -4,9 +4,17 @@ source .env
 
 docker run --rm \
     --network container:"$CONTAINER_NAME" \
+    "$COUCHBASE_IMAGE" /bin/sh -c '/opt/couchbase/bin/couchbase-cli bucket-create -c localhost:8091 \
+    -u Administrator -p password --bucket event_shop --bucket-type couchbase \
+    --bucket-ramsize 100 --bucket-replica 1 \
+    --wait'
+
+
+docker run --rm \
+    --network container:"$CONTAINER_NAME" \
     "$COUCHBASE_IMAGE" /bin/sh -c '/opt/couchbase/bin/cbq -u Administrator -p password <<EOF
-CREATE COLLECTION IF NOT EXISTS default._default.users;
-CREATE COLLECTION IF NOT EXISTS default._default.categories;
-CREATE COLLECTION IF NOT EXISTS default._default.locations;
-CREATE COLLECTION IF NOT EXISTS default._default.events;
+CREATE COLLECTION IF NOT EXISTS event_shop._default.users;
+CREATE COLLECTION IF NOT EXISTS event_shop._default.categories;
+CREATE COLLECTION IF NOT EXISTS event_shop._default.locations;
+CREATE COLLECTION IF NOT EXISTS event_shop._default.events;
 EOF'
