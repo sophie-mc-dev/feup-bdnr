d231ab248b9a2ed95fc7956f0c69ff55db6cea72
Milena Gouveia <milenaagouveia@gmail.com>
2024-04-19 20:47:58 +0100

    Add run-{backend, frontend, couchbase}.sh

---
+++ src/run-couchbase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo; echo "Running commands on server.sh"
+./server.sh
+
+echo; echo "Running commands on create.sh"
+./create.sh
+
+echo; echo "Running commands on populate.sh"
+./populate.sh
