412da9cd81ba06410a2a8cf257ddab897f703b7a
Francisco Prada <fmamp24@gmail.com>
2024-04-19 22:54:42 +0100

    refactored scripts

--- src/run-couchbase.sh
+++ src/run-couchbase.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+dir="./couchbase-setup"
+
+cd $dir || exit
 
 echo; echo "Running commands on server.sh"
 ./server.sh
 
+
 echo; echo "Running commands on create.sh"
 ./create.sh
+
 
 echo; echo "Running commands on populate.sh"
 ./populate.sh
