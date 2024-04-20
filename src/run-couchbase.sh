e1361966c065e778537acec399cd376c8612b861
Francisco Prada <fmamp24@gmail.com>
2024-04-20 11:55:58 +0100

    updated run_couchbase script

--- src/run-couchbase.sh
+++ src/run-couchbase.sh
@@ -6,10 +6,12 @@ cd $dir || exit
 echo; echo "Running commands on server.sh"
 ./server.sh
 
+sleep 5
 
 echo; echo "Running commands on create.sh"
 ./create.sh
 
+sleep 5
 
 echo; echo "Running commands on populate.sh"
 ./populate.sh
