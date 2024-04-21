49f77cd3bedfa564f2df01f125689e4dd8f2c5d8
Francisco Prada <fmamp24@gmail.com>
2024-04-21 17:50:26 +0100

    finished login and registration

--- src/my-app/backend/controllers/userController.js
+++ src/my-app/backend/controllers/userController.js
@@ -1,7 +1,66 @@
 const { connectToCouchbase } = require('../db/connection');
+const uuid = require('uuid');
 
 // login user
-async function loginUser(req, res) {} //TODO
+async function loginUser(req, res) {
+    if (!req.body.email && !req.body.password) {
+        return res.status(401).send({ "message": "An `email` and `password` are required" })
+    } else if (!req.body.email || !req.body.password) {
+        return res.status(401).send({ 
+          "message": `A ${!req.body.email ? '`email`' : '`password`'} is required`
+        })
+    }
+
+    const query = 'SELECT * FROM `users` WHERE email = $1 AND password = $2 OR username = $1 AND password = $2';
+    const params = [req.body.email, req.body.password];
+    const { usersCollection } = await connectToCouchbase();
+
+    try {
+        const result = await usersCollection.query(query, { parameters: params });
+        if (!result.rows.length) {
+            return res.status(401).send({ "message": "Invalid email/username or password" })
+        }
+        res.json(result.rows[0]);
+    }
+    catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+// register user
+async function registerUser(req, res) {
+    if (!req.body.email && !req.body.password) {
+        return res.status(401).send({ "message": "An `email` and `password` are required" })
+    } else if (!req.body.email || !req.body.password) {
+        return res.status(401).send({ 
+          "message": `A ${!req.body.email ? '`email`' : '`password`'} is required`
+        })
+    }
+
+    const id = uuid.v4()
+    const user = {
+        "user_id": id,
+        "name": req.body.name,
+        "username": req.body.username,
+        "email": req.body.email,
+        "password": req.body.password,
+        "is_organization": req.body.is_organization || false,
+        "liked_events": [],
+    }
+
+    const { usersCollection } = await connectToCouchbase();
+
+    try{
+        await usersCollection.upsert('new_user', user);
+        res.json(user);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+
+
+}
 
 // get user profile
 async function getUserById(req, res) {
@@ -23,5 +82,6 @@ async function getUserById(req, res) {
 
 module.exports = {
     loginUser,
+    registerUser,
     getUserById
 };
--- src/my-app/backend/routes/userRoutes.js
+++ src/my-app/backend/routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
-router.get('/', UserController.loginUser);
+router.get('/login', UserController.loginUser);
+
+router.get('/register', UserController.registerUser);
 
 // profile route
 router.get('/:user_id', UserController.getUserById);
