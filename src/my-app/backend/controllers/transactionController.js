e3daca169ef120bb16f3973b991dcaa7f42df1f0
Milena Gouveia <milenaagouveia@gmail.com>
2024-05-01 00:06:48 +0100

    Created routes to get upcoming and past tickets

--- src/my-app/backend/controllers/transactionController.js
+++ src/my-app/backend/controllers/transactionController.js
@@ -6,8 +6,7 @@ async function getPurchasesByUserId(req, res) {
         SELECT t.*, ARRAY_SUM(ARRAY item.quantity * item.ticket_price FOR item IN t.items END) AS total_price
         FROM transactions AS t
         WHERE user_id = $1 AND t.transaction_status = $2
-    `
-    ;
+    `;
 
     const options = {parameters: [user_id, 'purchased']}
 
@@ -20,7 +19,6 @@ async function getPurchasesByUserId(req, res) {
         res.status(500).send('Internal Server Error');
     }
 }
-
 
 async function getShoppingCartByUserId(req, res) {
     const user_id = req.params.user_id;
@@ -37,15 +35,37 @@ async function getShoppingCartByUserId(req, res) {
     }
 }
 
-async function getTicketsByUserId(req, res) {
+async function getUpcomingTicketsByUserId(req, res) {
     const user_id = req.params.user_id;
     const query = `
-      SELECT i.event_id, i.event_name, i.ticket_type, SUM(i.quantity) AS quantity
+      SELECT i.event_id, i.event_name, i.event_date, i.ticket_type, SUM(i.quantity) AS quantity
       FROM transactions AS t 
       UNNEST items AS i
-      WHERE t.user_id=$1 AND t.transaction_status=$2
-      GROUP BY i.event_id, i.ticket_type, i.event_name
-      ORDER BY i.event_name, i.ticket_type
+      WHERE t.user_id=$1 AND t.transaction_status=$2 AND MILLIS(i.event_date) >= NOW_MILLIS()
+      GROUP BY i.event_id, i.ticket_type, i.event_name, i.event_date
+      ORDER BY i.event_date, i.event_name
+    `;
+    const options = {parameters: [user_id, 'purchased']}
+  
+    try {
+        const { bucket } = await connectToCouchbase();
+        const result = await bucket.defaultScope().query(query, options);
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function getPastTicketsByUserId(req, res) {
+    const user_id = req.params.user_id;
+    const query = `
+      SELECT i.event_id, i.event_name, i.event_date, i.ticket_type, SUM(i.quantity) AS quantity
+      FROM transactions AS t 
+      UNNEST items AS i
+      WHERE t.user_id=$1 AND t.transaction_status=$2 AND MILLIS(i.event_date) < NOW_MILLIS()
+      GROUP BY i.event_id, i.ticket_type, i.event_name, i.event_date
+      ORDER BY i.event_date, i.event_name
     `;
     const options = {parameters: [user_id, 'purchased']}
   
@@ -62,5 +82,6 @@ async function getTicketsByUserId(req, res) {
 module.exports = {
     getPurchasesByUserId,
     getShoppingCartByUserId,
-    getTicketsByUserId
+    getUpcomingTicketsByUserId,
+    getPastTicketsByUserId
 };
--- src/my-app/backend/routes/transactionRoutes.js
+++ src/my-app/backend/routes/transactionRoutes.js
@@ -8,7 +8,10 @@ router.get('/purchases/:user_id', TransactionController.getPurchasesByUserId);
 // Shopping cart of a specific user
 router.get('/shopping_cart/:user_id', TransactionController.getShoppingCartByUserId);
 
-// Tickets of a specific user
-router.get('/tickets/:user_id', TransactionController.getTicketsByUserId);
+// Upcoming tickets of a specific user
+router.get('/tickets/upcoming/:user_id', TransactionController.getUpcomingTicketsByUserId);
+
+// Past tickets of a specific user
+router.get('/tickets/past/:user_id', TransactionController.getPastTicketsByUserId);
 
 module.exports = router;
