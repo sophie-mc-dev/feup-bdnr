83d41a5056591d986ea6b90827ba69adb7c7e849
Sofia Merino Costa <scosta.geprix@gmail.com>
2024-05-06 15:04:35 +0100

    UPDATE: comments page, comments in events page, dynamic add to cart button, small fixes

--- src/my-app/frontend/src/pages/EventPage.jsx
+++ src/my-app/frontend/src/pages/EventPage.jsx
@@ -60,17 +60,15 @@ const EventPage = () => {
         <Loading />
       ) : (
         <div className=" w-full p-20">
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row gap-5">
-              <h2 className="text-2xl font-semibold mb-6">
-                {eventInfo.event_name}
-              </h2>
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row gap-5 items-center mb-5">
+              <h2 className="text-4xl font-semibold">{eventInfo.event_name}</h2>
 
-              <div className="">
+              <div>
                 {eventInfo.categories.map((category, index) => (
                   <span
                     key={index}
-                    className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-600 mr-2 mb-2"
+                    className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-600 mr-2"
                   >
                     {category}
                   </span>
@@ -88,16 +86,16 @@ const EventPage = () => {
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
               </svg>
 
-              <p className="mb-4">{eventInfo.num_likes} likes</p>
+              <p className="mb-4 pr-4">{eventInfo.num_likes} likes</p>
             </div>
           </div>
 
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row text-sm gap-2 text-gray-600">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-6 h-6"
+              className="w-5 h-5"
             >
               <path
                 fillRule="evenodd"
@@ -110,12 +108,12 @@ const EventPage = () => {
             </p>
           </div>
 
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 text-sm text-gray-600">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-6 h-6"
+              className="w-5 h-5"
             >
               <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
               <path
@@ -129,22 +127,25 @@ const EventPage = () => {
 
           <p className="mb-4">{eventInfo.description}</p>
 
-          <h3 className="mt-5 mb-3 text-lg font-semibold">Ticket Types:</h3>
-          <div className="grid gap-4">
+          <h3 className="mt-10 mb-3 text-lg font-semibold">Ticket Types:</h3>
+          <div className="grid gap-4 p-4">
             {eventInfo.ticket_types.map((ticket, index) => (
               <TicketTypeCard key={index} ticket={ticket} />
             ))}
           </div>
 
-          <h3 className="mt-5 mb-3 text-lg font-semibold">Comments:</h3>
+          <h3 className="mt-10 mb-3 text-lg font-semibold">Comments:</h3>
           {isCommentsLoading ? (
             <Loading />
           ) : (
             <div>
               {comments.map((comment) => (
                 <div key={comment.comment_id} className="p-4">
-                  <p>User name: {comment.user_name}</p>
-                  <p>Text: {comment.text}</p>
+                  <p className="text-gray-800 font-semibold">
+                    {comment.user_name}
+                  </p>
+                  <p className="text-gray-700">{comment.text}</p>
+                  <hr className="my-4 border-t-2"/>
                 </div>
               ))}
             </div>
